86fdd0aaa940fe1e4f99ce5e662055db54c5d10c
Hunter Horsman <kariudo@gmail.com>
2026-04-22 21:10:30 +0000

    refactor(canary): add Microsoft Edge Canary Flatpak manifest and icon
    - Created a new Flatpak manifest file for Microsoft Edge Canary (com.microsoft.EdgeCanary.yaml) including build options, modules, and necessary runtime configurations.
    - Added an icon for Microsoft Edge Canary (com.microsoft.EdgeCanary-256.png) to the icons directory.

--- apply_extra.sh
+++ apply_extra.sh
@@ -5,8 +5,8 @@ set -e
 bsdtar -Oxf edge.deb 'data.tar*' |
   bsdtar -xf - \
     --strip-components=4 \
-    --exclude='./opt/microsoft/msedge-beta/nacl*' \
-    ./opt/microsoft/msedge-beta
+    --exclude='./opt/microsoft/msedge-canary/nacl*' \
+    ./opt/microsoft/msedge-canary
 rm edge.deb
 
 install -Dm755 /app/bin/stub_sandbox msedge-sandbox
